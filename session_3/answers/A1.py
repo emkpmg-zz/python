e51c40f5a27e01ac0c8219275bf8c89e8b6731b9
smirza <safia.mirza@kpmg.co.uk>
2019-10-07 13:34:12 +0100

    Added comments to answer

--- session_3/answers/A1.py
+++ session_3/answers/A1.py
@@ -1,7 +1,7 @@
 # A1 - Print 10 random numbers
 
-# first import the random module
+# import the random module
 import random
-# then create a for loop that will print a random number 10 times.
+# the loop will print a random number 10 times.
 for number in range(10):
     print(random.randint(1, 10))
