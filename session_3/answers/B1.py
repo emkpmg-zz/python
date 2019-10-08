a4273f1b6d72027177c35162f69d791529e8a301
smirza <safia.mirza@kpmg.co.uk>
2019-10-08 09:24:33 +0100

    Added pdf slides to session 4 and added answer to B1

--- session_3/answers/B1.py
+++ session_3/answers/B1.py
@@ -1,1 +1,7 @@
 # B1 - Create the following dictionary for an apple: Type = "Bramley", Price = 0.39, Colour = "Green".
+
+apple = {
+    "Type": "Bramley",
+    "Price": 0.39,
+    "Colour": "Green"
+}
