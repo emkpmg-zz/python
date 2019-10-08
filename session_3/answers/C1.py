984b9dee31baf4a43f3061374980693925071695
smirza <safia.mirza@kpmg.co.uk>
2019-10-08 15:28:54 +0100

    Added C1.py answer

--- session_3/answers/C1.py
+++ session_3/answers/C1.py
@@ -2,3 +2,16 @@
 # Store this information in a dictionary inside a list.
 # Continue to ask for names until no name is given.
 # Then print out all of the names and ages collected
+
+contacts = []
+fname = None
+while fname != "":
+    fname = input("What is your first name? ")
+    age = int(input("What is your age? "))
+    contacts.append(
+        {
+            "fname": fname,
+            "age": age
+        }
+    )
+print(contacts)
