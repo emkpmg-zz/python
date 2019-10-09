61e2c41d5114e8abe0d03ac436532478a2d12517
smirza <safia.mirza@kpmg.co.uk>
2019-10-09 15:32:02 +0100

    Updated .txt files and answers

--- session_5/answers/A2.py
+++ session_5/answers/A2.py
@@ -1,9 +1,6 @@
 # A2 - Read the file 'austen.txt' and print the amount of lines in the file
 
-fname = input("Enter file name: ")
-num_lines = 0
-with open(fname, 'r') as f:
-    for line in f:
-        num_lines += 1
-print("Number of lines:")
-print(num_lines)
+total = 0
+for x in open("austen.txt"):
+    total += 1
+print(total)
