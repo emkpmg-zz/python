b425a57ae41ecac8a8bb6f514f601e4dfaa5659a
smirza <safia.mirza@kpmg.co.uk>
2019-11-11 17:46:44 +0000

    Added answers to A5

--- session_4/answers/A5.py
+++ session_4/answers/A5.py
@@ -1,1 +1,7 @@
 # A5 - Write a function called 'print_list' that accepts a list as a parameter and then prints out each item of the list
+def print_list(names):
+    for row in names:
+        print(row)
+
+
+print_list(["Alice", "Bob", "Charlie"])
