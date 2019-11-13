34abff475ebb196ca9880ea43cbe6cf5d08e667b
smirza <safia.mirza@kpmg.co.uk>
2019-11-13 13:00:14 +0000

    Added answers to B3.py

--- session_4/answers/B3.py
+++ session_4/answers/B3.py
@@ -4,3 +4,15 @@
 # ****
 # **
 # *
+
+
+def print_stars(x):
+    star = ""
+    for stars in range(0, x):
+        star = star + "*"
+    print(star)
+    if x > 1:
+        print_stars(x-1)
+
+
+print_stars(50)
